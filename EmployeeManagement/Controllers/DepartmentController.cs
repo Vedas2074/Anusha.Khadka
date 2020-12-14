f97d5a415ca93d05cd42b0bdeb4d599330e473ab
anushakhadka <aanukhadka4@gmail.com>
2020-12-14 15:53:42 +0545

    Departments

--- EmployeeManagement/Controllers/DepartmentController.cs
+++ EmployeeManagement/Controllers/DepartmentController.cs
@@ -10,35 +10,20 @@ namespace EmployeeManagement.Controllers
 {
     public class DepartmentController : Controller
     {
-         public IActionResult Index()
+        public IActionResult Index()
         {
-            Department department1 = new Department()
-            {
-                Id = 103,
-                DepartmentName = "Finance",
-                DepartmentNo = 770,
-                Floor = "3rd",
-                Salary = 3000000
-            };
-            Department department2 = new Department()
-            {
-                Id = 104,
-                DepartmentName = "HR",
-                DepartmentNo = 800,
-                Floor = "1st",
-                Salary = 8900000
-            };
-            Department department3 = new Department()
-            {
-                Id = 105,
-                DepartmentName = "Marketing",
-                 DepartmentNo = 900,
-                 Floor = "2nd",
-                Salary = 8500000
-            };
-            List<Department> departments =  new List<Department>() {department1, department2,department3};
+        
+            var department = Department.GetDepartments();
 
-            return View(departments);
+            return View(department);
+        }
+        
+        public ActionResult Detail(int id)
+        {
+            var departments = Department.GetDepartments();
+            var emp = departments.FirstOrDefault(x => x.Id == id);
+            return View(emp);
+
         }
         public ActionResult Add()
         {
--- EmployeeManagement/Models/Department.cs
+++ EmployeeManagement/Models/Department.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 namespace EmployeeManagement.Models
 {
     public class Department
@@ -11,8 +12,39 @@ namespace EmployeeManagement.Models
          public int DepartmentNo {get;  set;}
         
             public string Floor {get;  set;}
-
-
         public float Salary { get; set;}
+
+
+        public static List<Department> GetDepartments()
+            {Department department1 = new Department()
+            {
+                Id = 103,
+                DepartmentName = "Finance",
+                DepartmentNo = 770,
+                Floor = "3rd",
+                Salary = 3000000
+            };
+            Department department2 = new Department()
+            {
+                Id = 104,
+                DepartmentName = "HR",
+                DepartmentNo = 800,
+                Floor = "1st",
+                Salary = 8900000
+            };
+            Department department3 = new Department()
+            {
+                Id = 105,
+                DepartmentName = "Marketing",
+                 DepartmentNo = 900,
+                 Floor = "2nd",
+                Salary = 8500000
+            };
+            List<Department> departments =  new List<Department>() {department1, department2,department3};
+
+            return departments;
+            }
     }
 }
+    
+
